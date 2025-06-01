f1153c19353dd49bed0fc07a10fc2df3f187c75d
Mikael <mikael.hatanpaa@gmail.com>
2025-06-01 21:05:40 +0200

    Some folder reorg

--- 001-intro-to-vue-3/L2/main.js
+++
@@ -1,8 +0,0 @@
-const app = Vue.createApp({
-  data() {
-    return {
-      product: "Boots",
-      description: "A pair of very nice boots",
-    };
-  },
-});
--- 001-intro-to-vue-3/L9/main.js
+++
@@ -1,9 +0,0 @@
-const app = Vue.createApp({
-  data() {
-    return {
-      cart: 0,
-      premium: true,
-    };
-  },
-  methods: {},
-});
