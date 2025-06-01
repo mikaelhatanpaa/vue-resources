6effa11dcbf53c2a3a01cd67094d3fe4ee0307bf
Mikael <mikael.hatanpaa@gmail.com>
2025-06-01 22:11:12 +0200

    finalised chapter 5

--- 002-basics-real-world-vue-composition-api/src/views/EventListView.vue
+++ 002-basics-real-world-vue-composition-api/src/views/EventListView.vue
@@ -1,42 +1,17 @@
 <script setup>
 import EventCard from "../components/EventCard.vue"
-import {ref} from "vue"
+import { ref, onMounted } from "vue"
+import EventService from "../services/EventService.js"
 
-const events = ref(
-  [
-    {
-      id: 5928101,
-      category: 'animal welfare',
-      title: 'Cat Adoption Day',
-      description: 'Find your new feline friend at this event.',
-      location: 'Meow Town',
-      date: 'January 28, 2022',
-      time: '12:00',
-      petsAllowed: true,
-      organizer: 'Kat Laydee'
-    },
-    {
-      id: 4582797,
-      category: "food",
-      title: "Community Gardening",
-      description: "Join us as we tend to the community edible",
-      location: "Flora City",
-      date: "March 14, 2022",
-      time: "10:00",
-      petsAllowed: true,
-      organizer: "Fern Pollin"
-  },
-  {
-    id: 8419988,
-    category: "sustainability",
-    title: "Beach Cleanup",
-    description: "Help pick up trash along the shore.",
-    location: "Playa Del Carmen",
-    date: "July 22, 2022",
-    time: "11:00",
-    petsAllowed: false,
-    organizer: "Carey Wales"
-  }])
+const events = ref(null)
+onMounted(() => {
+  EventService.getEvents()
+    .then((response) => {
+      events.value = response.data
+      console.log("@RESPONSE: ", response)
+    })
+    .catch((error) => console.log("@ERROR: ", error))
+})
 </script>
 
 <template>
@@ -47,7 +22,6 @@ const events = ref(
 </template>
 
 <style scoped>
-
 .events {
   display: flex;
   flex-direction: column;
